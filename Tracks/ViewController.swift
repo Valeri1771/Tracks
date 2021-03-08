e635719e9304e6ed5c97fb25809b9d661e25a558
Valeri1771 <leri1771@gmail.com>
2021-03-08 15:25:29 +0300

    Initial Commit

---
+++ Tracks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracks
+//
+//  Created by Valeri Tsyganova on 08.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
